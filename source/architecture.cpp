9155f036ab555f2b0813cb1b0d3ff170a394a238
yankaic <yankaic.as@gmail.com>
2017-11-14 13:02:51 -0300

    v0.2.1 Impressao do mapa e definicao dos estados iniciais

--- source/architecture.cpp
+++ source/architecture.cpp
@@ -1,5 +1,6 @@
-
+#include<iostream>
 #include "Test.cpp"
+#include "board.cpp"
 void run();
 
 
@@ -11,8 +12,18 @@ int main(){
 		run();	
 }
 
-
+void printMap(char* text){	
+	std::cout << "line: " << text << std::endl;
+	for(int line = 0; line < BOARD_SIZE; line++){
+		for(int column = BOARD_SIZE-1; column >= 0; column--){
+			char ch = text[line*BOARD_SIZE+column];
+			std::cout <<(ch == '1'? " X" : " ·");
+		}		
+		std::cout << std::endl;
+	}
+}
 
 void run(){
-	
+	printMap(toString(whitePieces));
 }
+
--- source/board.hpp
+++ source/board.hpp
@@ -1,7 +1,7 @@
 
 typedef unsigned long long Map;
 #define BOARD_SIZE 8 
-#define OCCUPIED_SQUARES whitePieces | blackPieces
+#define OCCUPIED_SQUARES ((whitePieces | blackPieces))
 
 struct Square{
 	int line;
@@ -18,20 +18,20 @@ struct Movement{
 	Square destination;
 };
 
-Map whitePieces = 0x0;
-Map blackPieces = 0x0;
+Map whitePieces 	= 0x000000000000FFFD;
+Map blackPieces		= 0xFFFF000000000000;
 
-Map pawnPieces = 0x0;
-Map rookPieces = 0x0;
-Map bishopPieces = 0x0;
-Map knightPieces = 0x0;
-Map queenPieces = 0x0;
-Map kingPieces = 0x0;
+Map pawnPieces 		= 0x00FF00000000FF00;
+Map rookPieces 		= 0x8100000000000081;
+Map knightPieces	= 0x4200000000000042;
+Map bishopPieces 	= 0x2400000000000024;
+Map queenPieces 	= 0x1000000000000010;
+Map kingPieces 		= 0x0800000000000008;
 
 Map 		map(int line, int column); 
 Map 		map(Square square);  
 Square 	getLocation(Map map);
-void 		print(Map map);  
+char*		toString(Map map);  
 int 		count(Map map);  
 
 Map northAttacks		(Map occupied, Square square);
@@ -50,4 +50,5 @@ Map bishopAttacks  	(Map occupied, Square square);
 Map knightAttacks		(Map occupied, Square square); 
 Map queenAttacks   	(Map occupied, Square square);
 Map kingAttacks   	(Map occupied, Square square);
+
 
